58c8318da27ae5d273593e82eb06f67d1d043560
Franke266 <ifranjic099@gmail.com>
2023-01-19 19:26:31 +0100

    user auth added

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,37 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Login from "./Login";
-import Checkout from "./Checkout";
+// import Checkout from "./Checkout";
+import { auth } from "./firebase";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { useStateValue } from "./StateProvider";
+import reducer, { initialState, actionTypes } from "./reducer";
+import { StateProvider } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <div className="app">
       <BrowserRouter>
@@ -15,6 +41,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/" element={<Home />}></Route>
+          {/* <Route path="/checkout" element={<Checkout />}></Route> */}
         </Routes>
       </BrowserRouter>
     </div>
