802dc27d9fa29c80b6036dd219a3aa47a742b335
Franke266 <ifranjic099@gmail.com>
2023-01-23 18:09:40 +0100

    categories added

---
+++ src/Category.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useReducer } from "react";
+import "./Category.css";
+import reducer, { initialState, actionTypes } from "./reducer";
+import { useGlobalContext } from "./StateProvider";
+interface ICategoryProps {
+  id: string;
+  title: string;
+  image: string;
+}
+
+export default function Category({ id, title, image }: ICategoryProps) {
+  const { state, dispatch } = useGlobalContext();
+
+  return (
+    <div className="category">
+      <div className="category_title">
+        <p>{title}</p>
+      </div>
+      <img src={image} alt="" />
+    </div>
+  );
+}
