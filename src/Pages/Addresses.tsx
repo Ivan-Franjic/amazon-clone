c82d722e7704a89d782a2f94e20569551904faf6
Franke266 <ifranjic099@gmail.com>
2023-02-22 22:06:08 +0100

    components imported

--- src/Pages/Addresses.tsx
+++ src/Pages/Addresses.tsx
@@ -1,3 +1,13 @@
+import Address from "../Components/Address/Address";
+import AddAddress from "../Components/Address/AddAddress";
 export default function Addresses() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-start ml-16">
+      <p className="flex lg:py-5 lg:text-3xl">Your Addresses</p>
+      <div className="flex justify-center gap-4 mt-2">
+        <AddAddress />
+        <Address />
+      </div>
+    </div>
+  );
 }
