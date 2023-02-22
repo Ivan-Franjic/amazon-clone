c4dab35457ce49a14ac56f2a39f3e44093e5b7ac
Franke266 <ifranjic099@gmail.com>
2023-02-22 10:58:52 +0100

    navbar links redesigned

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,17 +1,9 @@
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { Link } from "react-router-dom";
-import { signOut } from "firebase/auth";
-import { auth } from "../../Common/firebase";
 import { useGlobalContext } from "../../Common/StateProvider";
 export default function Header() {
   const { state, dispatch } = useGlobalContext();
-
-  const handleAuthentication = () => {
-    signOut(auth);
-  };
-
-  console.log(state.user);
 
   return (
     <div className="flex items-center top-0 sticky z-50 h-14 bg-lblue lg:bg-blue">
@@ -33,35 +25,46 @@ export default function Header() {
 
       <div className="flex justify-evenly">
         {state.user ? (
-          <Link to={"/login"}>
-            <div
-              onClick={handleAuthentication}
-              className="flex flex-col h-12 p-1 mx-2.5 text-white hover:border-solid hover:border-white hover:border-x hover:border-y"
-            >
-              <span className="text-xs leading-5 text-left">
-                Hello, {state.user.email}
-              </span>
-              <span className="text-sm font-extrabold">{"Sign Out"}</span>
-            </div>
-          </Link>
+          <>
+            <Link to={"/account"}>
+              <div className="flex flex-col h-12 p-1 mx-2.5 text-white hover:border-solid hover:border-white hover:border-x hover:border-y">
+                <span className="text-xs leading-5 text-left">
+                  Hello, {state.user.email}
+                </span>
+                <span className="text-sm font-extrabold">{"Account"}</span>
+              </div>
+            </Link>
+            <Link to="/orders">
+              <div className="hidden h-12 p-1 mx-2.5 text-white hover:border-solid hover:border-white hover:border-x hover:border-y lg:flex lg:flex-col">
+                <span className="text-xs leading-5 text-left">Returns</span>
+                <span className="text-sm font-extrabold text-left">
+                  & Orders
+                </span>
+              </div>
+            </Link>
+          </>
         ) : (
-          <Link to={"/login"}>
-            <div className="flex flex-col h-12 p-1 mx-2.5 text-white hover:border-solid hover:border-white hover:border-x hover:border-y">
-              <span className="text-xs leading-5 text-left">
-                Hello, {"Guest"}
-              </span>
-              <span className="text-sm font-extrabold text-left">
-                {"Sign In"}
-              </span>
-            </div>
-          </Link>
+          <>
+            <Link to={"/login"}>
+              <div className="flex flex-col h-12 p-1 mx-2.5 text-white hover:border-solid hover:border-white hover:border-x hover:border-y">
+                <span className="text-xs leading-5 text-left">
+                  Hello, {"Guest"}
+                </span>
+                <span className="text-sm font-extrabold text-left">
+                  {"Sign In"}
+                </span>
+              </div>
+            </Link>
+            <Link to="/login">
+              <div className="hidden h-12 p-1 mx-2.5 text-white hover:border-solid hover:border-white hover:border-x hover:border-y lg:flex lg:flex-col">
+                <span className="text-xs leading-5 text-left">Returns</span>
+                <span className="text-sm font-extrabold text-left">
+                  & Orders
+                </span>
+              </div>
+            </Link>
+          </>
         )}
-        <Link to="/orders">
-          <div className="hidden h-12 p-1 mx-2.5 text-white hover:border-solid hover:border-white hover:border-x hover:border-y lg:flex lg:flex-col">
-            <span className="text-xs leading-5 text-left">Returns</span>
-            <span className="text-sm font-extrabold text-left">& Orders</span>
-          </div>
-        </Link>
 
         <Link to="/checkout">
           <div className="flex flex-col h-12 p-1 items-center text-white hover:border-solid hover:border-white hover:border-x hover:border-y lg:flex lg:flex-row">
