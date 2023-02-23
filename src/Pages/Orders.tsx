b69f5f6c5114b09849876aaf1facf79936385f86
Franke266 <ifranjic099@gmail.com>
2023-02-23 11:28:05 +0100

    order component imported

--- src/Pages/Orders.tsx
+++ src/Pages/Orders.tsx
@@ -1,3 +1,10 @@
+import Order from "../Components/Order/Order";
 export default function Orders() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center my-10 gap-4">
+      <p className="flex lg:py-5 lg:text-3xl">Your Orders</p>
+      <Order />
+      <Order />
+    </div>
+  );
 }
