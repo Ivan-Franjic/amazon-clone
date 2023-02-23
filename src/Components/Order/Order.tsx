adc47d2f35504b91abd2c10f7f40068b2c79c76e
Franke266 <ifranjic099@gmail.com>
2023-02-23 15:07:07 +0100

    design changes

--- src/Components/Order/Order.tsx
+++ src/Components/Order/Order.tsx
@@ -1,7 +1,7 @@
 export default function Order() {
   return (
-    <div className="flex flex-col w-40 h-48 bg-white border border-gray shadow rounded-lg lg:w-3/5 lg:h-80">
-      <div className="flex px-5 border-b gap-5 border-gray shadow bg-lgray">
+    <div className="flex flex-col w-40 h-48 bg-white border border-gray rounded-lg lg:w-3/5 lg:h-80">
+      <div className="flex px-5 border-b gap-5 border-gray bg-lgray">
         <div className="flex flex-col text-xs tracking-tight text-dgray my-2.5 lg:text-sm">
           ORDER PLACED
           <p>30 January 2023</p>
