c06585015ee55162cdeefb7bdbfc73f16e089822
Franke266 <ifranjic099@gmail.com>
2023-02-23 20:31:36 +0100

    design changes

--- src/Pages/Account.tsx
+++ src/Pages/Account.tsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 export default function Account() {
   return (
     <div className="flex flex-col items-center">
-      <p className="flex lg:p-5 lg:text-3xl">Your Account</p>
-      <div className="flex gap-2 lg:gap-5">
+      <p className="flex p-5 font-semibold lg:text-3xl lg:font-normal">
+        Your Account
+      </p>
+      <div className="flex flex-col gap-2 lg:flex-row lg:gap-5">
         <Link to={`/account/orders`}>
-          <div className="flex w-28 h-10 bg-white border border-gray rounded-lg lg:w-80 lg:h-24 hover:bg-lgray">
+          <div className="flex w-80 h-20 bg-white border border-gray rounded-lg lg:h-24 hover:bg-lgray">
             <img
-              className="flex w-6 h-6 mx-2 mt-2 rounded-full lg:mx-5 lg:mt-5 lg:w-12 lg:h-12"
+              className="flex w-10 h-10 mx-2 mt-5 rounded-full lg:mx-5 lg:w-12 lg:h-12"
               src="https://firebasestorage.googleapis.com/v0/b/clone-e98ba.appspot.com/o/orders_icon.png?alt=media&token=d5c2a28d-a815-4206-afbb-1d05d5df84ae"
               alt="product image"
             />
@@ -23,9 +25,9 @@ export default function Account() {
         </Link>
 
         <Link to={`/account/login&security`}>
-          <div className="flex w-28 h-10 bg-white border border-gray rounded-lg lg:w-80 lg:h-24 hover:bg-lgray">
+          <div className="flex w-80 h-20 bg-white border border-gray rounded-lg lg:h-24 hover:bg-lgray">
             <img
-              className="flex w-6 h-6 mx-2 mt-2 rounded-full lg:mx-5 lg:mt-5 lg:w-12 lg:h-12"
+              className="flex w-10 h-10 mx-2 mt-5 rounded-full lg:mx-5 lg:w-12 lg:h-12"
               src="https://firebasestorage.googleapis.com/v0/b/clone-e98ba.appspot.com/o/security_icon.png?alt=media&token=362c1f02-8052-4f01-a444-237c1538024d"
               alt="product image"
             />
@@ -41,9 +43,9 @@ export default function Account() {
         </Link>
 
         <Link to={`/account/addresses`}>
-          <div className="flex w-28 h-10 bg-white border border-gray rounded-lg lg:w-80 lg:h-24 hover:bg-lgray">
+          <div className="flex w-80 h-20 bg-white border border-gray rounded-lg lg:h-24 hover:bg-lgray">
             <img
-              className="flex w-6 h-6 mx-2 mt-2 rounded-full lg:mx-5 lg:mt-5 lg:w-12 lg:h-12"
+              className="flex w-10 h-10 mx-2 mt-5 rounded-full lg:mx-5 lg:w-12 lg:h-12"
               src="https://firebasestorage.googleapis.com/v0/b/clone-e98ba.appspot.com/o/address_icon.png?alt=media&token=2e311e65-d370-4661-8548-ada841643e39"
               alt="product image"
             />
@@ -52,7 +54,7 @@ export default function Account() {
                 Your Addresses
               </h5>
               <p className="text-xs tracking-tight text-dgray lg:text-sm">
-                Edit addresses and delivery preferences
+                Edit addresses and delivery options
               </p>
             </div>
           </div>
--- src/Pages/Addresses.tsx
+++ src/Pages/Addresses.tsx
@@ -3,7 +3,9 @@ import AddAddress from "../Components/Address/AddAddress";
 export default function Addresses() {
   return (
     <div className="flex flex-col items-center ml-2 mt-2 lg:items-start lg:ml-16">
-      <p className="flex lg:py-5 lg:text-3xl">Your Addresses</p>
+      <p className="flex py-5 font-semibold lg:text-3xl lg:font-normal">
+        Your Addresses
+      </p>
       <div className="flex flex-col justify-center gap-4 mt-2 lg:flex-row">
         <AddAddress />
         <Address />
