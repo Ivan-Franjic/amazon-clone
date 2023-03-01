2d8674d3cf55f57fc97d5e86106877f8e982c563
Franke266 <ifranjic099@gmail.com>
2023-03-01 18:11:52 +0100

    user orders retrieved from db

--- src/Components/Order/Order.tsx
+++ src/Components/Order/Order.tsx
@@ -1,24 +1,35 @@
-export default function Order() {
+import { IOrderData } from "../../Types/Order.type";
+export default function Order({
+  id,
+  name,
+  items,
+  order_date,
+  total,
+}: IOrderData) {
   return (
     <div className="flex flex-col w-11/12 h-48 bg-white border border-gray rounded-lg lg:w-2/4 lg:h-80">
       <div className="flex px-5 border-b gap-5 border-gray bg-lgray">
         <div className="flex flex-col text-xs tracking-tight text-dgray my-2.5 lg:text-sm">
           ORDER PLACED
-          <p>30 January 2023</p>
+          <p>{order_date}</p>
         </div>
         <div className="flex flex-col text-xs tracking-tight text-dgray my-2.5 lg:text-sm">
           TOTAL
-          <p>9.00$</p>
+          <p>${total}</p>
         </div>
         <div className="flex flex-col text-xs tracking-tight text-dgray my-2.5 lg:text-sm">
           DISPATCH TO
-          <p>Name Surname</p>
+          <p>{name}</p>
         </div>
       </div>
       <div className="flex flex-col px-5 mt-2">
-        <p className="flex text-xs font-bold tracking-tight text-black mb-0.5 lg:text-lg">
-          Arriving by 1 March
-        </p>
+        {items.map((item: any) => (
+          <>
+            <p>{item.name}</p>
+            <p>{item.description}</p>
+            <p>{item.price}</p>
+          </>
+        ))}
       </div>
     </div>
   );
