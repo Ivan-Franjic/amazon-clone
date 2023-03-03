235733282254d8eb22494f908a7ac3a8bb774560
Franke266 <ifranjic099@gmail.com>
2023-03-03 10:50:56 +0100

    design changes

--- src/Components/Order/Order.tsx
+++ src/Components/Order/Order.tsx
@@ -7,7 +7,7 @@ export default function Order({
   total,
 }: IOrderData) {
   return (
-    <div className="flex flex-col w-11/12 h-48 bg-white border border-gray rounded-lg lg:w-2/4 lg:h-80">
+    <div className="flex flex-col w-11/12 h-48 bg-white border border-gray rounded-lg lg:w-2/4 lg:h-content">
       <div className="flex px-5 border-b gap-5 border-gray bg-lgray">
         <div className="flex flex-col text-xs tracking-tight text-dgray my-2.5 lg:text-sm">
           ORDER PLACED
@@ -22,13 +22,20 @@ export default function Order({
           <p>{name}</p>
         </div>
       </div>
-      <div className="flex flex-col px-5 mt-2">
+      <div className="flex flex-col px-5 mt-2 overflow-y-scroll">
         {items.map((item: any) => (
-          <>
-            <p>{item.name}</p>
-            <p>{item.description}</p>
-            <p>{item.price}</p>
-          </>
+          <div key={item.id} className="flex justify-between">
+            <div className="flex my-3">
+              <div className="w-28 h-28s">
+                <img className="object-contain" src={item.image} />
+              </div>
+              <div className="flex flex-col ml-2">
+                <strong>{item.name}</strong>
+                <p>{item.description}</p>
+              </div>
+            </div>
+            <strong className="mt-3">${item.price}</strong>
+          </div>
         ))}
       </div>
     </div>
